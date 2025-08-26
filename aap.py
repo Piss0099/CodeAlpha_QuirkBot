7e8f4f2e5a3aabb74d98a10f97ed538a083f0301
Ravi Choudhary <129587687+Piss0099@users.noreply.github.com>
2025-08-26 21:24:39 +0530

    Import aap.py

--- aap.py
+++ aap.py
@@ -1,2 +1,14 @@
 # CodeAlpha_QuirkBot
-A Code Alpha internship Project 2nd Task
+
+
+# QuirkBot: Friendly FAQ Chatbot
+# Author: Ravi Choudhary
+# Internship: CodeAlpha AI Internship (2025)
+
+import json
+import streamlit as st
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
